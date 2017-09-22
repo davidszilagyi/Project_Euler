d167e094fdc6b02c90c1cf3d056e29f6d508501d
David Szilagyi <szilagyidav@gmail.com>
2017-09-22 19:38:36 +0200

    Problem 3 solved

---
+++ Problem_Solver/Problem_3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler.Problem_Solver
+{
+    class Problem_3
+    {
+        public dynamic Calculate(dynamic to)
+        {
+            dynamic k = 2;
+            while (k * k <= to)
+            {
+                if(to % k == 0)
+                {
+                    to /= k;
+                }
+                else
+                {
+                    ++k;
+                }
+            }
+            return to;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Project_Euler.Problem_Solver;
 using System;
+using System.Collections.Generic;
 
 namespace Project_Euler
 {
@@ -8,11 +9,14 @@ namespace Project_Euler
         static void Main(string[] args)
         {
             Console.WriteLine("Problem 1:");
-            Problem_1 p1 = new Problem_1();           
+            Problem_1 p1 = new Problem_1();
             Console.WriteLine(p1.Multiples(1000, 3, 5));
             Console.WriteLine("\nProblem 2:");
             Problem_2 p2 = new Problem_2();
             Console.WriteLine(p2.EvenFibonacci(4000000));
+            Console.WriteLine("\nProblem 3:");
+            Problem_3 p3 = new Problem_3();
+            Console.WriteLine(p3.Calculate(600851475143));
             Console.ReadKey();
         }
     }
