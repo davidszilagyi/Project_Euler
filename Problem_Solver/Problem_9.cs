750f40479f953a3c39aed44da7b16a3f6f142e4c
David Szilagyi <szilagyidav@gmail.com>
2017-09-22 22:45:01 +0200

    Problem 9 solved

---
+++ Problem_Solver/Problem_9.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler.Problem_Solver
+{
+    class Problem_9
+    {
+        public long FindABC(int number)
+        {
+            int[] result = new int[3];
+            for (int a = 0; a < number / 2; a++)
+            {
+                for (int b = 0; b < number / 2; b++)
+                {
+                    int c = number - a - b;
+                    if (IsCorrect(a, b, c) && a + b + c == number && a < b && b < c)
+                    {
+                        result[0] = a;
+                        result[1] = b;
+                        result[2] = c;
+                    }
+                }
+            }
+            return result[0] * result[1] * result[2];
+        }
+
+        private bool IsCorrect(int a, int b, int c)
+        {
+            return a * a + b * b == c * c;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,30 +8,33 @@ namespace Project_Euler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Problem 1:");
-            Problem_1 p1 = new Problem_1();
-            Console.WriteLine(p1.Multiples(1000, 3, 5));
-            Console.WriteLine("\nProblem 2:");
-            Problem_2 p2 = new Problem_2();
-            Console.WriteLine(p2.EvenFibonacci(4000000));
-            Console.WriteLine("\nProblem 3:");
-            Problem_3 p3 = new Problem_3();
-            Console.WriteLine(p3.Calculate(600851475143));
-            Console.WriteLine("\nProblem 4:");
-            Problem_4 p4 = new Problem_4();
-            Console.WriteLine(p4.PalindromeNumber(999, 100));
-            Console.WriteLine("\nProblem 5:");
-            Problem_5 p5 = new Problem_5();
-            Console.WriteLine(p5.SmallestNumber(1, 20));
-            Console.WriteLine("\nProblem 6:");
-            Problem_6 p6 = new Problem_6();
-            Console.WriteLine(p6.Squares(1, 100));
-            Console.WriteLine("\nProblem 7:");
-            Problem_7 p7 = new Problem_7();
-            Console.WriteLine(p7.WhatIsThePrime(10001));
-            Console.WriteLine("\nProblem 8:");
-            Problem_8 p8 = new Problem_8();
-            Console.WriteLine(p8.Largest(13));
+            //Console.WriteLine("Problem 1:");
+            //Problem_1 p1 = new Problem_1();
+            //Console.WriteLine(p1.Multiples(1000, 3, 5));
+            //Console.WriteLine("\nProblem 2:");
+            //Problem_2 p2 = new Problem_2();
+            //Console.WriteLine(p2.EvenFibonacci(4000000));
+            //Console.WriteLine("\nProblem 3:");
+            //Problem_3 p3 = new Problem_3();
+            //Console.WriteLine(p3.Calculate(600851475143));
+            //Console.WriteLine("\nProblem 4:");
+            //Problem_4 p4 = new Problem_4();
+            //Console.WriteLine(p4.PalindromeNumber(999, 100));
+            //Console.WriteLine("\nProblem 5:");
+            //Problem_5 p5 = new Problem_5();
+            //Console.WriteLine(p5.SmallestNumber(1, 20));
+            //Console.WriteLine("\nProblem 6:");
+            //Problem_6 p6 = new Problem_6();
+            //Console.WriteLine(p6.Squares(1, 100));
+            //Console.WriteLine("\nProblem 7:");
+            //Problem_7 p7 = new Problem_7();
+            //Console.WriteLine(p7.WhatIsThePrime(10001));
+            //Console.WriteLine("\nProblem 8:");
+            //Problem_8 p8 = new Problem_8();
+            //Console.WriteLine(p8.Largest(13));
+            Console.WriteLine("\nProblem 9:");
+            Problem_9 p9 = new Problem_9();
+            Console.WriteLine(p9.FindABC(1000));
             Console.ReadKey();
         }
     }
