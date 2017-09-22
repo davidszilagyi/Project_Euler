796d92491f20f9c7e64a5800d8ac7acc8987b112
David Szilagyi <szilagyidav@gmail.com>
2017-09-22 23:20:00 +0200

    Problem 10 solved

---
+++ Problem_Solver/Problem_10.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler.Problem_Solver
+{
+    class Problem_10
+    {
+        public long SumOfPrimes(long max)
+        {
+            long result = 2;
+            for (int i = 3; i <= max; i += 2)
+            {
+                if (IsPrime(i))
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+
+        private bool IsPrime(long number)
+        {
+            for (int i = 3; (i * i) <= number; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
