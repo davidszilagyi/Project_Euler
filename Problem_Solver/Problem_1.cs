218663c59190540f9b5a9f4dca75c8ee2429c78d
David Szilagyi <szilagyidav@gmail.com>
2017-09-22 14:15:41 +0200

    added folder to problem solver

--- Problem_Solver/Problem_1.cs
+++ Problem_Solver/Problem_1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project_Euler
+namespace Project_Euler.Problem_Solver
 {
     class Problem_1
     {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_Euler.Problem_Solver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
